d9561d488d58c045e4893aea29680786dcc3bf9e
KarenLiberato <101460087+KarenLiberato@users.noreply.github.com>
2022-03-31 19:51:24 -0500

    Add files via upload

--- Fiesta.py
+++ Fiesta.py
@@ -1,9 +1,12 @@
-ingreso=str(input("número de personas que asistieron": ))
+ingreso=str(input("desea ingresar?:  " ))
 ch=0
 cm=0
+pro_h=0
+pro_m=0
 acu_h=0
 acu_m=0
 e_min=0
+
 if ingreso=="si" or ingreso=="si" or ingreso=="si":
     
     while ingreso=="si" or ingreso=="si" or ingreso=="si":
@@ -16,31 +19,31 @@ if ingreso=="si" or ingreso=="si" or ingreso=="si":
             nombre=str(input("nombre completo:  "))
             genero=str(input("genero:  "))
             
-            while e_min<edad and edad !=0:
+            
+            if e_min<edad and edad !=0:
                 ed=edad
-                if condition:
-                    
-                    
-            if genero=="hombre"
-                ch +=1
                 
+                    
+                    
+                if genero=="hombre":
+                    ch +=1
                 acu_h=acu_h+edad
-                pro_h=int(acu_h\ch)
-            elif genero=="mujer":
+                pro_h=int(acu_h/ch)
+        if genero=="mujer":
                 cm +=1
                 acu_m=acu_m+edad
                 pro_m=int(acu_m/cm)
-            elif edad>0 and edad<18:
+        elif edad>0 and edad<18:
                 print("no puede ingresar por que no cuenta con la edad requerida")
-                else:
+        else:
                     break
-        print(f"""\ntotal asistentes de la fiesta: {t_h_m}\ntotal de aistentes hombres: {ch}.\ntotal de mujeres: {cm})
+    print(f"""\ntotal asistentes de la fiesta: {t_h_m}\ntotal de aistentes hombres: {ch}.\ntotal de mujeres: {cm})
                     \npromedio edad hombre: {pro_h} \npromedio edad mujeres: {pro_m}
                     \nedad minima de personas que asistieron:  {ed}""")
                 
             
-                else:
-                    print("salto de pagina")
+else:
+    print("salir de pagina")
                     
         
  
